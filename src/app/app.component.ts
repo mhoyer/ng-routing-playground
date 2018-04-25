c15fa9aea48b70b858827f7f228098581500001b
Marcel Hoyer <m.hoyer@cid.com>
2018-04-25 08:14:18 +0200

    simplified generated `AppComponent`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>AppComponent</h1>
+  <router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  title = 'app';
 }
