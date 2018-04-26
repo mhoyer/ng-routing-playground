8da8db5f9d8c5b77e1b88f8340af1795b26abe15
Marcel Hoyer <m.hoyer@cid.com>
2018-04-26 16:58:30 +0200

    add loading animation demo to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>AppComponent</h1>
-  <router-outlet></router-outlet>`,
+  template: `
+    <div [hidden]="!loading" class="loader">
+      <h2>Loading...</h2>
+    </div>
+    <div [hidden]="loading" class="router-output">
+      <h1>AppComponent</h1>
+      <router-outlet></router-outlet>
+    </div>`
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  loading = true;
+
+  constructor(private router: Router) { }
+
+  ngAfterViewInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd || event instanceof NavigationCancel) {
+        this.loading = false;
+      }
+    });
+  }
 }
