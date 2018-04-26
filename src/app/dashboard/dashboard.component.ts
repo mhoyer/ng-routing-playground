8d23f4d9408a4dc96d3226caae495869b4664efb
Marcel Hoyer <m.hoyer@cid.com>
2018-04-26 16:58:15 +0200

    add [restart] link to DashboardComponent

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  template: `<h3>Dashboard</h3>`
+  template: `<h3>Dashboard</h3>
+    <a href="/">restart</a>`
 })
 export class DashboardComponent {
 }
